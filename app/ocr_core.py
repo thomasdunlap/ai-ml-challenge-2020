08ed8f9d65acca2def384a2eda40e0626536419c
thomasdunlap <tom.a.dunlap@gmail.com>
2020-08-16 11:02:46 -0700

    Streaming clause prediction works. Need to fine tune clause extraction better than by line break.  Need to add predictions and confidence values to web UI. Need to add docx support.

--- app/ocr_core.py
+++ app/ocr_core.py
@@ -1,21 +1,28 @@
-from PIL import Image, ImageEnhance, ImageFilter
 import cv2
 import glob
 import pytesseract
 import time
+from transformers import pipeline
+from transformers import PretrainedConfig, TFBertForSequenceClassification, BertTokenizer
+
+
+MODEL_FOLDER = './static/models/'
 UPLOAD_PATH = './static/uploads/'
+
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True, add_special_tokens=True,
+                                                max_length=512, pad_to_max_length=True)
+config = PretrainedConfig.from_json_file(MODEL_FOLDER + 'config.json')
+model = TFBertForSequenceClassification.from_pretrained(MODEL_FOLDER)
+
 
 def ocr_core(filename):
     """
     This function will handle the core OCR processing of images.
     """
-    #image = convert_from_path(filename)
     print(filename)
-    #image = convert_to_image(UPLOAD_PATH + filename, UPLOAD_PATH + filename[:-4])
     im = cv2.imread(filename)
     img_rgb = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
     text = pytesseract.image_to_string(img_rgb, config='-c preserve_interword_spaces=1') 
-    #text = pytesseract.image_to_string(Image.open(filename))  # We'll use Pillow's Image class to open the image and pytesseract to detect the string in the image
     return text  # Then we will print the text in the images
 
 def multi_doc_pipeline(filename):
@@ -25,9 +32,11 @@ def multi_doc_pipeline(filename):
         print(f'extracting text from {im_path}')
         extracted_text = ocr_core(im_path)
         print('text extracted')
-        
-        yield extracted_text
-        time.sleep(1)                                    
+        for clause in extracted_text.split('\n\n'):
+            prediction = pipeline('sentiment-analysis', model=model, tokenizer=tokenizer, config=config)(clause)
+            print(prediction)
+            yield clause
+            time.sleep(1)                                    
 
 if __name__ == '__main__':
     print(ocr_core('test'))
